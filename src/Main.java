a71b8196f7f48eb7428c0879c777e9e47f81dcf6
3yebmb <3yebmb@gmail.com>
2019-01-17 04:06:31 +0500

    pre-release

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        new Method().go(1);
+        new Method().go(1);
         new Method().go(2);
     }
 }

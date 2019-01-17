bc8d0573ff7f58b7e157fb8cecca81ce371571d0
3yebmb <3yebmb@gmail.com>
2019-01-17 11:40:55 +0500

    corrected separate method and created union method

--- src/Method.java
+++ src/Method.java
@@ -1,11 +1,11 @@
 public class Method {
-    final int size = 10000000; // количество элементов в исходном массиве
+    final int size = 100; // количество элементов в исходном массиве
     final int countThread = 11; // количество потоков для второго метода
     int h = size / countThread; // размерность подмассива
     float[][] subArr = new float[countThread][]; // массив с массивами (:
     int last = h; // рзамерноть последнего массива
     int pos = 0; // смещение
-    long timeOperation; // время потраченное на операцию
+    long timeOperation, timeAllOperations; // время потраченное на операцию
     ThreadClass[] threads = new ThreadClass[countThread]; // массив с объектами потока
 
     final float[] arr = new float[size]; // исходный массив
@@ -27,8 +27,8 @@ public class Method {
                 System.out.println("На заполнение элементов массива новыми значениями 1 потоку потребовалось " + timeOperation + " мс");
                 break;
             case 2 :
-                timeOperation = System.currentTimeMillis();
-                prepareArray();
+                timeAllOperations = timeOperation = System.currentTimeMillis();
+                separateArray();
                 timeOperation = System.currentTimeMillis() - timeOperation;
                 System.out.println("На разбиение массива на " + countThread + " частей ушло " + timeOperation + " мс");
                 timeOperation = System.currentTimeMillis();
@@ -41,16 +41,36 @@ public class Method {
                 timeOperation = System.currentTimeMillis() - timeOperation;
                 System.out.println("На заполнение элементов массива новыми значениями " + countThread + " потокам потребовалось " + timeOperation + " мс");
 
+                timeOperation = System.currentTimeMillis();
+                unionSubArray(subArr);
+                timeOperation = System.currentTimeMillis() - timeOperation;
+                timeAllOperations = System.currentTimeMillis() - timeAllOperations;
+                System.out.println("На слияние " + countThread + " подмассивов, потребовалось " + timeOperation + " мс");
+                System.out.println("Общее время выполнения разделения, обработки и склейки обратно : " + timeAllOperations + " мс");
                 break;
         }
     }
 
-    // метод разделения массива на равные части
-    private void prepareArray(){
+    private void unionSubArray(float[][] subArr){
+        pos = 0;
+        for (float[] s: subArr){
+            System.arraycopy(s, pos, arr, 0, s.length);
+            pos += s.length;
+        }
+        System.out.println(java.util.Arrays.deepToString(new float[][]{arr}));
+    }
 
-        if ((l !=0)){
-            h += 1;
-            last = (arr.length % ((countThread-1)*h));
+    // метод разделения массива на равные части
+    private void separateArray(){
+
+        if (l !=0){
+            if ((arr.length - (h*countThread)<0)) {
+                h += 1;
+                last = (arr.length % ((countThread - 1) * h));
+            }
+            else
+                last = arr.length - (h*countThread);
+
         }
 
         for (int i = 0; i < countThread-1; i++) {
@@ -62,6 +82,6 @@ public class Method {
         subArr[countThread-1] = new float[last];
         System.arraycopy(arr, pos, subArr[countThread-1], 0, last);
         pos = 0;
-//        System.out.println(java.util.Arrays.deepToString(new float[][]{subArr[countThread-1]}));
+        System.out.println(java.util.Arrays.deepToString(new float[][]{subArr[countThread-1]}));
     }
 }
